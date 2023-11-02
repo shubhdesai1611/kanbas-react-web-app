113fd92bb70c9bc0b47f11ce0b58cc01fa87b599
shubhdesai1611 <126991856+shubhdesai1611@users.noreply.github.com>
2023-11-02 19:11:31 -0400

    Labs Updated

--- src/Labs/a4/ReduxExamples/index.js
+++ src/Labs/a4/ReduxExamples/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import HelloRedux from "./HelloRedux";
+import AddRedux from "./AddRedux";
+import CounterRedux from "./CounterRedux";
+import TodoList from "./todos/TodoList";
 
 const ReduxExamples = () => {
   return (
     <div>
       <h2>Redux Examples</h2>
+      <HelloRedux />
+      <AddRedux />
+      <CounterRedux />
+      <TodoList />
     </div>
   );
 };
